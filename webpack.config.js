9d36d5b6a3c0a32d0e6d62692c1e666df9c84da6
Roger <luojie.5408@163.com>
2019-07-15 16:38:38 +0800

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,19 @@
+const webpack = require('webpack')
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.min.js",
         path: __dirname + "/dist"
     },
-    
     mode:'development',
-    
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
-
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
-
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
@@ -26,23 +23,38 @@ module.exports = {
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
-
     plugins: [
         new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             template: './index.html'
         })
     ],
-
     optimization : {
         splitChunks: {
             cacheGroups: {
                 vendor:{
-                    chunks:"all",
                     test: /node_modules\/react-dom/, // 把react-dom抽出来，比较大
-                    name:"vendor",
+                    chunks:"all",
+                    name:"vendor"
                 },
             }
         }
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        host:'0.0.0.0', //外部服务器可以访问
+        disableHostCheck: true,
+        inline: true,
+        hot: true,
+        open:true, //是否运行时打开浏览器
+        // compress:true, //是否压缩
+        // port:8080, //端口号
+        // proxy: {
+        //     '/': {
+        //         target: 'http://172.16.1.25:9090/',
+        //         changeOrigin: true
+        //     }
+        // }    
     }
 }
